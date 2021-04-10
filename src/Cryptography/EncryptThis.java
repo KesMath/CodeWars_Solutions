11a0af035ff2e07fee8a76119bbd23b81da04b57
unknown <kesmathieu30@gmail.com>
2021-04-10 19:49:57 -0400

    patching functions for edge cases

--- src/Cryptography/EncryptThis.java
+++ src/Cryptography/EncryptThis.java
@@ -2,21 +2,30 @@ package Cryptography;
 
 public class EncryptThis {
     public static String encryptThis(String text) {
+        //FIXME: NOT FULLY TESTED FOR ALL EDGE CASES
+        if(text.length() == 0){ return ""; }
         String [] words = text.split(" ");
         StringBuilder str = new StringBuilder();
         for(String word: words){
             StringBuilder w = new StringBuilder(word);
-            char temp = w.charAt(1);
-            char letter = w.charAt(0);
-            w.setCharAt(1, w.charAt(w.length()-1));
-            w.setCharAt(w.length()-1, temp);
-            w.deleteCharAt(0);
-            str.append(String.valueOf((int) letter) + w + " ");
+            if(w.length() >= 2) {
+                char temp = w.charAt(1);
+                char letter = w.charAt(0);
+                w.setCharAt(1, w.charAt(w.length() - 1));
+                w.setCharAt(w.length() - 1, temp);
+                w.deleteCharAt(0);
+                str.append(String.valueOf((int) letter) + w + " ");
+            }
+            else{
+                str.append(Integer.toString(text.charAt(0)) + " ");
+            }
         }
         return str.toString().trim();
     }
 
     public static String decryptThis(String text) {
+        //FIXME: NOT FULLY TESTED FOR ALL EDGE CASES
+        if(text.length() == 0){ return ""; }
         String[] words = text.split(" ");
         StringBuilder str = new StringBuilder();
         for (String word : words) {
@@ -28,20 +37,25 @@ public class EncryptThis {
                     digits.append(word.charAt(i));
                     w.deleteCharAt(0);
                 }
+                else{
+                    break;
+                }
             }
-            char temp = w.charAt(0);
-            w.setCharAt(0, w.charAt(w.length() - 1));
-            w.setCharAt(w.length() - 1, temp);
+            if(w.length() >= 1) {
+                char temp = w.charAt(0);
+                w.setCharAt(0, w.charAt(w.length() - 1));
+                w.setCharAt(w.length() - 1, temp);
+            }
             str.append(Character.toString(Integer.parseInt(digits.toString())) + w + " ");
-            }
+        }
         return str.toString().trim();
     }
 
     public static void main(String[] args) {
-        String str = "Thank you Piotr for all your help";
+        String str = "A wise old owl lived in an oak";
         String encryptedStr = EncryptThis.encryptThis(str);
-        System.out.println(encryptedStr.equals("84kanh 121uo 80roti 102ro 97ll 121ruo 104ple"));
-        //System.out.println(EncryptThis.decryptThis(encryptedStr));
+        System.out.println(encryptedStr.equals("65 119esi 111dl 111lw 108dvei 105n 97n 111ka"));
+        System.out.println(EncryptThis.decryptThis(encryptedStr));
         System.out.println(EncryptThis.decryptThis(encryptedStr).equals(str));
     }
 }
