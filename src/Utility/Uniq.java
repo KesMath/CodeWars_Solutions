61738c7a4651569aec2d0543fcff8cfe18d27436
unknown <kesmathieu30@gmail.com>
2021-07-18 18:47:13 -0400

    adding soln for 'uniq -c' cli tool

---
+++ src/Utility/Uniq.java
@@ -0,0 +1,41 @@
+package Utility;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Uniq {
+    /**
+     * Implement a function which behaves like the 'uniq -c' command in UNIX.
+     *
+     * It takes as input a sequence and returns a sequence in which all duplicate
+     * elements following each other have been reduced to one instance together
+     * with the number of times a duplicate elements occurred in the original array.
+     * @param arr of characters that should be received via CLI
+     * @return list of dictionaries containing number of consecutive occurrences
+     */
+    public static List<HashMap<Character, Integer>> uniq(char[] arr){
+        List<HashMap<Character, Integer>> list = new ArrayList();
+        int cout = 1;
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i] == arr[i+1]){
+                cout++;
+            }
+            else{
+                HashMap<Character, Integer> map = new HashMap<>();
+                map.put(arr[i], cout);
+                list.add(map);
+                cout = 1;
+            }
+        }
+        //need to add trailing array additions
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put(arr[arr.length - 1], cout);
+        list.add(map);
+        return list;
+    }
+    public static void main(String[] args) {
+        System.out.println(Uniq.uniq(new char[]{'a','a','b','b','c','a','b','c'}));
+        System.out.println(Uniq.uniq(new char[]{'a','a','a','b','b','b','c','c','c'}));
+    }
+}
