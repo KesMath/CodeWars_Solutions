447b9877ec865293d4369db59f85b130e9279962
unknown <kesmathieu30@gmail.com>
2021-11-07 02:52:19 -0500

    modifying code to generate and cache all md5 permutations of pins comprised of 5 digits

--- src/Cryptography/CrackThePin.java
+++ src/Cryptography/CrackThePin.java
@@ -14,29 +14,26 @@ import java.security.NoSuchAlgorithmException;
 
 public class CrackThePin {
     /**
-     *This function permutes all md5 hashes for a 5 digit pin (each digit ranging from 0 - 9)
-     * and returns the pin corresponding to the parametrized string. This exercise
+     *This function permutes all md5 hashes for a 5 digit pin (ranging from 0 - 9) This exercise
      * is used to simulate one technique of cracking md5 hashing through brute forcing.
      *
-     * The real world context of this exercise can be used to create rainbow tables
-     * which are precomputed tables for caching passwords to their hash functions.
-     *
      * Given the password policy of a web site at time of password creation,
+     * (best bet would be to leverage Wayback Machine: https://archive.org/web/ and try to create account)
      * along with a reference to password hashes (assuming no salt), an attacker can
      * use a function as below to reverse engineer the actual password
      *
-     * @param hash - md5 hash
-     * @return 5 digit pin, that when applied through md5 hash function, returns parameter str
+     * @param
+     * @return - string containing all 100,000 md5 permutations for a pin comprised of 5 digits
      */
-    private String convertByteArrToHex(byte [] arr){
+     public static String convertByteArrToHex(byte [] arr){
         StringBuilder hex = new StringBuilder();
         for(byte b : arr){
             hex.append(String.format("%02x", b));
         }
         return hex.toString();
     }
-    public String crack(String hash) throws NoSuchAlgorithmException {
-        String pin = null;
+    public String permuteAllMD5Hashes() throws NoSuchAlgorithmException {
+        StringBuilder builder = new StringBuilder();
         MessageDigest md;
         try{
             md = MessageDigest.getInstance("MD5");
@@ -44,28 +41,30 @@ public class CrackThePin {
         catch (NoSuchAlgorithmException e){
             throw new NoSuchAlgorithmException(e);
         }
-        label:
         for(int f = 0; f < 10; f++){
             for(int g = 0; g < 10; g++){
                 for(int h = 0; h < 10; h++){
                     for(int i = 0; i < 10; i++){
                         for(int j = 0; j < 10; j++){
                             byte[] arr = md.digest(("" + f + ""+ g + "" + h + "" + i + "" + j).getBytes());
-                            if(convertByteArrToHex(arr).equals(hash)){
-                                pin = ("" + f + ""+ g + "" + h + "" + i + "" + j);
-                                break label;
+                            builder.append("" + f +
+                                           "" + g +
+                                           "" + h +
+                                           "" + i +
+                                           "" + j +
+                                           "," + convertByteArrToHex(arr) + "\n");
                             }
                         }
                     }
                 }
             }
+        return builder.toString();
         }
-        return pin;
-    }
-
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        CrackThePin cracker = new CrackThePin();
-        cracker.crack("827ccb0eea8a706c4c34a16891f84e7b");
-        cracker.crack("86aa400b65433b608a9db30070ec60cd");
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        CrackThePin pins = new CrackThePin();
+        FileWriter myWriter = new FileWriter("src\\Cryptography\\rainbow_table.csv");
+        myWriter.write("5 Digit PIN, MD5 HASH\n");
+        myWriter.write(pins.permuteAllMD5Hashes());
+        myWriter.close();
     }
 }
