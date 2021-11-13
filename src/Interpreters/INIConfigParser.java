329d4268ed00d782bb3cf16c35abd2739dfc35c6
unknown <kesmathieu30@gmail.com>
2021-11-13 16:24:51 -0500

    adding commentary

--- src/Interpreters/INIConfigParser.java
+++ src/Interpreters/INIConfigParser.java
@@ -1,5 +1,33 @@
 package Interpreters;
 
+//DESCRIPTION: https://www.codewars.com/kata/59d168926bddd2ff46000030
+
+
+/**
+ * In this kata, you'll have to parse an INI file, a common configuration format used by many applications.
+ * In an INI file, config data is stored in key-value pairs with optional sections, like so:
+ *
+ * ****************
+ * key1=value1   **
+ * key2=value2   **
+ *               **
+ * [section]     **
+ * key3=value3   **
+ * key4=value4   **
+ * ****************
+ *
+ * YIELDS:
+ * ************************
+ * {                     **
+ *   "key1": "value1",   **
+ *   "key2": "value2",   **
+ *   "section": {        **
+ *     "key3": "value3", **
+ *     "key4": "value4"  **
+ *   }                   **
+ * }                     **
+ * ************************
+ */
 public class INIConfigParser implements ConfigParser {
 
 
