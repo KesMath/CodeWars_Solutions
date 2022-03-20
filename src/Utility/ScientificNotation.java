313a371e13d7fc46dc8f371093649401b0b1fc98
unknown <kesmathieu30@gmail.com>
2022-03-19 21:26:13 -0400

    adding logic for base case

--- src/Utility/ScientificNotation.java
+++ src/Utility/ScientificNotation.java
@@ -2,13 +2,25 @@ package Utility;
 
 public class ScientificNotation {
 
-    public static String shorthand(int num){
-        return null;
+    public static String shorthand(float num){
+        int cout = 0;
+        if (num > 0){
+            while(num > 10){
+                num /= 10;
+                cout++;
+            }
+        }
+        else{
+
+        }
+
+        String s = Float.toString(num) + "*10^" + Integer.toString(cout);
+        return s;
     }
 
     public static void main(String[] args) {
         System.out.println(ScientificNotation.shorthand(5000000)); // returns "5*10^6"
         System.out.println(ScientificNotation.shorthand(532000));  // returns "5.32*10^5"
-        System.out.println(ScientificNotation.shorthand(-2000));   // returns "2*10^3"
+        //System.out.println(ScientificNotation.shorthand(-2000));   // returns "-2*10^3"
     }
 }
