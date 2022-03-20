006d2a0c18959be761dab072feb6b6b641ea42c4
unknown <kesmathieu30@gmail.com>
2022-03-19 21:03:54 -0400

    adding test cases

--- src/Utility/ScientificNotation.java
+++ src/Utility/ScientificNotation.java
@@ -7,9 +7,8 @@ public class ScientificNotation {
     }
 
     public static void main(String[] args) {
-        System.out.println(ScientificNotation.shorthand(5000000));
-        System.out.println(ScientificNotation.shorthand(532000));
-        System.out.println(ScientificNotation.shorthand(5000000));
-        System.out.println(ScientificNotation.shorthand(5000000));
+        System.out.println(ScientificNotation.shorthand(5000000)); // returns "5*10^6"
+        System.out.println(ScientificNotation.shorthand(532000));  // returns "5.32*10^5"
+        System.out.println(ScientificNotation.shorthand(-2000));   // returns "2*10^3"
     }
 }
