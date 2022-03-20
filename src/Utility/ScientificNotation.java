cb95553a092edb89bbf6b587062ed7f09e6c1589
unknown <kesmathieu30@gmail.com>
2022-03-19 20:41:45 -0400

    adding template for scientific notation

---
+++ src/Utility/ScientificNotation.java
@@ -0,0 +1,15 @@
+package Utility;
+
+public class ScientificNotation {
+
+    public static String shorthand(int num){
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ScientificNotation.shorthand(5000000));
+        System.out.println(ScientificNotation.shorthand(532000));
+        System.out.println(ScientificNotation.shorthand(5000000));
+        System.out.println(ScientificNotation.shorthand(5000000));
+    }
+}
