772aa17451f521e4956496235e2ab5ad6f75dd4b
unknown <kesmathieu30@gmail.com>
2022-04-10 17:29:37 -0400

    adding one machine state and testing result

--- src/Theoretical/VendingMachineFiniteStateAutomata.java
+++ src/Theoretical/VendingMachineFiniteStateAutomata.java
@@ -4,20 +4,30 @@ import java.util.HashMap;
 
 public class VendingMachineFiniteStateAutomata {
     private HashMap<String, Integer> wordsToNumbers = new HashMap<>();
-    int candyPrice = 25;
+    private HashMap<Integer, HashMap<Integer,String>> finiteMachineStates = new HashMap<>();
+    int candyPriceBalance = 25;
 
     public VendingMachineFiniteStateAutomata(){
         wordsToNumbers.put("five", 5);
         wordsToNumbers.put("ten", 10);
         wordsToNumbers.put("twenty", 20);
+
+        finiteMachineStates.put(25, new HashMap<>()
+                                {{
+                                    put(5, "twenty");
+                                    put(10, "fifteen");
+                                    put(20, "five");
+                                }});
     }
 
     public String vendingMachine(String amount){
         if(!wordsToNumbers.containsKey(amount)){
             return "I don't know what you put inside of me, but I'm keeping it";
         }
-        return "";
-
+        Integer insertedValue = wordsToNumbers.get(amount);
+        String newBalance = finiteMachineStates.get(candyPriceBalance).get(insertedValue);
+        candyPriceBalance -= insertedValue;
+        return "Give me " + newBalance + " more";
     }
 
     public static void main(String[] args) {
