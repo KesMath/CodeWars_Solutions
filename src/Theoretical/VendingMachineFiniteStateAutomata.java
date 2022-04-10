3f046157ea708afe04bdee3cc46f189811b4d5b1
unknown <kesmathieu30@gmail.com>
2022-04-10 18:00:42 -0400

    adding more states to deal with all scenarios

--- src/Theoretical/VendingMachineFiniteStateAutomata.java
+++ src/Theoretical/VendingMachineFiniteStateAutomata.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class VendingMachineFiniteStateAutomata {
     private HashMap<String, Integer> wordsToNumbers = new HashMap<>();
     private HashMap<Integer, HashMap<Integer,String>> finiteMachineStates = new HashMap<>();
-    int candyPriceBalance = 25;
+    private int candyPriceBalance = 25;
 
     public VendingMachineFiniteStateAutomata(){
         wordsToNumbers.put("five", 5);
@@ -18,6 +18,30 @@ public class VendingMachineFiniteStateAutomata {
                                     put(10, "fifteen");
                                     put(20, "five");
                                 }});
+
+        finiteMachineStates.put(20, new HashMap<>()
+                                {{
+                                    put(5, "fifteen");
+                                    put(10, "ten");
+                                    put(20, "Take your candy");
+                                }});
+
+        finiteMachineStates.put(15, new HashMap<>()
+                                {{
+                                    put(5, "ten");
+                                    put(10, "five");
+                                }});
+
+        finiteMachineStates.put(10, new HashMap<>()
+                                {{
+                                    put(5, "five");
+                                    put(10, "Take your candy");
+                                }});
+
+        finiteMachineStates.put(5, new HashMap<>()
+                                {{
+                                    put(5, "Take your candy");
+                                }});
     }
 
     public String vendingMachine(String amount){
@@ -27,12 +51,16 @@ public class VendingMachineFiniteStateAutomata {
         Integer insertedValue = wordsToNumbers.get(amount);
         String newBalance = finiteMachineStates.get(candyPriceBalance).get(insertedValue);
         candyPriceBalance -= insertedValue;
+        if(candyPriceBalance == 0){
+            return newBalance;
+        }
         return "Give me " + newBalance + " more";
     }
 
     public static void main(String[] args) {
         VendingMachineFiniteStateAutomata vm = new VendingMachineFiniteStateAutomata();
+        System.out.println(vm.vendingMachine("twenty"));
         System.out.println(vm.vendingMachine("five"));
-        System.out.println(vm.vendingMachine("o"));
+        //System.out.println(vm.vendingMachine("o"));
     }
 }
