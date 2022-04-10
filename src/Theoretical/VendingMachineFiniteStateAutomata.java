ad5815a7ccbc2fc6e68333c4547f99af1840c456
unknown <kesmathieu30@gmail.com>
2022-04-10 18:59:04 -0400

    finalizing logic

--- src/Theoretical/VendingMachineFiniteStateAutomata.java
+++ src/Theoretical/VendingMachineFiniteStateAutomata.java
@@ -1,17 +1,20 @@
 package Theoretical;
-
 import java.util.HashMap;
 
+//DESCRIPTION: https://www.codewars.com/kata/59267e389b424dcd3f0000c9/train/javascript
 public class VendingMachineFiniteStateAutomata {
     private HashMap<String, Integer> wordsToNumbers = new HashMap<>();
+    private HashMap<Integer, String> numbersToWords = new HashMap<>();
     private HashMap<Integer, HashMap<Integer,String>> finiteMachineStates = new HashMap<>();
     private int candyPriceBalance = 25;
 
     public VendingMachineFiniteStateAutomata(){
+        //indicates which amounts are accepted by machine
         wordsToNumbers.put("five", 5);
         wordsToNumbers.put("ten", 10);
         wordsToNumbers.put("twenty", 20);
 
+        //for state transitions until a balance reaches 0
         finiteMachineStates.put(25, new HashMap<>()
                                 {{
                                     put(5, "twenty");
@@ -42,6 +45,15 @@ public class VendingMachineFiniteStateAutomata {
                                 {{
                                     put(5, "zero");
                                 }});
+
+        //required in cases if user gives more than balance
+        numbersToWords.put(5, "five");
+        numbersToWords.put(10, "ten");
+        numbersToWords.put(15, "fifteen");
+    }
+
+    public void resetBalance(){
+        this.candyPriceBalance = 25;
     }
 
     public String vendingMachine(String amount){
@@ -52,15 +64,24 @@ public class VendingMachineFiniteStateAutomata {
         String newBalance = finiteMachineStates.get(candyPriceBalance).get(insertedValue);
         candyPriceBalance -= insertedValue;
         if(candyPriceBalance == 0){
+            resetBalance();
             return "Take your candy";
+        }
+        else if(candyPriceBalance < 0){
+            String debtOwed = numbersToWords.get(candyPriceBalance * -1);
+            resetBalance();
+            return "Take your candy and " + debtOwed + " back";
         }
         return "Give me " + newBalance + " more";
     }
 
     public static void main(String[] args) {
         VendingMachineFiniteStateAutomata vm = new VendingMachineFiniteStateAutomata();
-        System.out.println(vm.vendingMachine("twenty"));
         System.out.println(vm.vendingMachine("five"));
-        //System.out.println(vm.vendingMachine("o"));
+        System.out.println(vm.vendingMachine("five"));
+        System.out.println(vm.vendingMachine("twenty"));
+        System.out.println(vm.vendingMachine("twenty"));
+        System.out.println(vm.vendingMachine("o"));
+        System.out.println(vm.vendingMachine("ten"));
     }
 }
